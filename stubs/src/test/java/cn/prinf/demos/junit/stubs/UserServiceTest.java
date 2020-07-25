8320c865b5ddcea29601f2f3732cc29f76c33df2
linksgo2011 <120377843@qq.com>
2020-07-25 13:15:52 +0800

    add Mockito Core as dependency

--- stubs/src/test/java/cn/prinf/demos/junit/stubs/UserServiceTest.java
+++ stubs/src/test/java/cn/prinf/demos/junit/stubs/UserServiceTest.java
@@ -3,11 +3,15 @@ package cn.prinf.demos.junit.stubs;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class UserServiceTest {
 
     @Test
     public void should_register() {
-//        new UserService();
+        UserRepository mockedUserRepository = mock(UserRepository.class);
+        EmailService mockedEmailService = mock(EmailService.class);
+        EncryptionService mockedEncryptionService = mock(EncryptionService.class);
+        new UserService(mockedUserRepository,mockedEmailService,mockedEncryptionService);
     }
 }
