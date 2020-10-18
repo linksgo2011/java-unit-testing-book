8abb5344fffe7db6a217e8fca409d490aa09d723
lin <120377843@qq.com>
2020-10-18 18:00:27 +0800

    add arch unit demo

---
+++ archunit/src/test/java/cn/printf/demos/archunit/ArchUnitTest.java
@@ -0,0 +1,8 @@
+package cn.printf.demos.archunit;
+
+
+@RunWith(ArchUnitRunner.class) // Remove this line for JUnit 5!!
+@AnalyzeClasses(packages = "cn.printf.demos.archunit")
+public class ArchUnitTest {
+
+}
