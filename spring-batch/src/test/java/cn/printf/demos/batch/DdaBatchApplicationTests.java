b11bca7bdbab4e679b5c1592c035e387b1e2dc55
lin <120377843@qq.com>
2020-10-31 19:35:58 +0800

    add spring batch application

---
+++ spring-batch/src/test/java/cn/printf/demos/batch/DdaBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.printf.demos.batch;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdaBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
